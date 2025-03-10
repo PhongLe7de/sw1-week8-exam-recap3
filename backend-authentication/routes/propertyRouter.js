7e5b67c611996f8eec26cbbb912a52b2cd855e15
PhongLe7de <phongle260260@gmail.com>
2025-03-10 12:51:11 +0200

    Protect routes

--- backend-authentication/routes/propertyRouter.js
+++ backend-authentication/routes/propertyRouter.js
@@ -7,10 +7,14 @@ const {
   updateProperty,
   deleteProperty,
 } = require("../controllers/propertyControllers");
+const requireAuth = require("../middleware/requireAuth");
 
 const router = express.Router();
 
 router.get("/", getAllProperties);
+
+router.use(requireAuth)
+
 router.post("/", createProperty);
 router.get("/:propertyId", getPropertyById);
 router.put("/:propertyId", updateProperty);
